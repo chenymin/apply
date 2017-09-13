ec40e7a365576b440d0bb9807a004459f01087d8
micky <912072089@qq.com>
2017-09-13 15:59:19 +0800

    * [apply] 初始化赋值

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,7 +3,7 @@ import _ from 'lodash'
 const defaultKey = {myInput: 'value', mySelect: 'defaultVal', myDistPicker: 'defaultVal'}
 export const bindDefaultValue = (state, key) => {
   const info = state.global.currentData[key]
-  _.assign(state.apply.applyEdit, {})
+  state.apply.applyEdit = {}
   _.forEach(info, ({type, props, model}) => {
     if (model) {
       state.apply.applyEdit[model] = props[defaultKey[type]] || ''
